b3c2e4c48b89e3ffbccbdf22af4fc3ea08f35101
Giovanny <giovanny.tapiero@pi.edu.co>
2024-10-06 17:54:21 -0500

    Agregar archivos de proyecto.

---
+++ SentenciaIfElseIf/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SentenciaIfElseIf
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+
+            bool areYouHungry = true;
+            bool youHaveMoney = false;
+            TimeSpan time2 = new TimeSpan(12, 0, 0);
+
+            Console.WriteLine("Hola fundillo");
+            if (areYouHungry && youHaveMoney && AbiertoOcerradoFund("Lonches alejo", time2))
+            {
+                Console.WriteLine("abierto");
+            }
+            else
+            {
+                Console.WriteLine("cerrado"+ "Hora de: "+ time2 + "");
+            }
+        }
+
+        static bool AbiertoOcerradoFund(string name, TimeSpan time)
+        {
+            TimeSpan time3 = new TimeSpan(08, 0, 0);
+            TimeSpan time4 = new TimeSpan(24, 0, 0);
+
+            if (name == "Lonches alejo" && time == (time3) && time < time4 )
+            {
+                Console.WriteLine("se fue de taxi");
+                return true;
+            }
+            else if (name == "Fundillo") 
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("No habia caldo de pata");
+                return true;
+            }
+        }
+    }
+}
