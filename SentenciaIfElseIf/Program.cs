c109840f017a05a91dc4c884d316d8946bad7283
Giovanny <giovanny.tapiero@pi.edu.co>
2024-10-06 19:08:04 -0500

    commit 3

--- SentenciaIfElseIf/Program.cs
+++ SentenciaIfElseIf/Program.cs
@@ -1,46 +1,123 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
-namespace SentenciaIfElseIf
+class SnakeGame
 {
-    class Program
+    static int ancho = 20; // Ancho del tablero
+    static int alto = 20; // Alto del tablero
+    static List<(int, int)> snake = new List<(int, int)>(); // Lista de posiciones de la serpiente
+    static (int, int) direccion = (0, 1); // Dirección inicial (movimiento hacia la derecha)
+    static (int, int) comida; // Posición de la comida
+    static Random random = new Random();
+
+    static void Main()
     {
-     
-        static void Main(string[] args)
+        // Inicializar la serpiente
+        snake.Add((alto / 2, ancho / 2));
+
+        // Generar la primera comida
+        GenerarComida();
+
+        // Configurar el bucle de juego
+        while (true)
         {
+            DibujarTablero();
+            InputUsuario();
+            MoverSerpiente();
 
-            bool variable_a = true;
-            TimeSpan time2 = new TimeSpan(08, 0, 0);
+            // Controlar la velocidad del juego
+            Thread.Sleep(500); // Reducir el valor para aumentar la velocidad
+        }
+    }
 
-            Console.WriteLine("Hola tienda deportiva: ");
-            if (variable_a && AbiertoOcerradoLocal("ropa y accesorios", time2))
+    static void DibujarTablero()
+    {
+        Console.Clear();
+        for (int i = 0; i < alto; i++)
+        {
+            for (int j = 0; j < ancho; j++)
             {
-                Console.WriteLine("hora de apertura: "+ time2);
+                if (i == 0 || i == alto - 1 || j == 0 || j == ancho - 1)
+                {
+                    Console.Write("#"); // Bordes del tablero
+                }
+                else if (snake.Contains((i, j)))
+                {
+                    Console.Write("O"); // Cuerpo de la serpiente
+                }
+                else if ((i, j) == comida)
+                {
+                    Console.Write("X"); // Comida
+                }
+                else
+                {
+                    Console.Write(" "); // Espacios vacíos
+                }
             }
-            else
+            Console.WriteLine();
+        }
+    }
+
+    static void InputUsuario()
+    {
+        if (Console.KeyAvailable)
+        {
+            var tecla = Console.ReadKey(true).Key;
+            switch (tecla)
             {
-                Console.WriteLine("cerrado"+ "Hora de ciere: "+ time2 + "");
+                case ConsoleKey.UpArrow:
+                    direccion = (-1, 0);
+                    break;
+                case ConsoleKey.DownArrow:
+                    direccion = (1, 0);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    direccion = (0, -1);
+                    break;
+                case ConsoleKey.RightArrow:
+                    direccion = (0, 1);
+                    break;
             }
         }
+    }
 
-        static bool AbiertoOcerradoLocal(string name, TimeSpan time)
+    static void MoverSerpiente()
+    {
+        var cabeza = snake[0];
+        var nuevaCabeza = (cabeza.Item1 + direccion.Item1, cabeza.Item2 + direccion.Item2);
+
+        // Verificar colisiones con el borde del tablero o con el propio cuerpo
+        if (nuevaCabeza.Item1 <= 0 || nuevaCabeza.Item1 >= alto - 1 || nuevaCabeza.Item2 <= 0 || nuevaCabeza.Item2 >= ancho - 1 || snake.Contains(nuevaCabeza))
         {
-            TimeSpan time3 = new TimeSpan(08, 0, 0);
-            TimeSpan time4 = new TimeSpan(24, 0, 0);
-
-            if (name == "ropa y accesorios" && time == (time3) && time < time4 )
-            {
-                Console.WriteLine("local abierto, en el centro Comercial !, ");
-                return true;
-            }
-            else if (name == "No es ropa y accesorios") 
-            {
-                return false;
-            }
-            else
-            {
-                Console.WriteLine("Promocion no completada");
-                return true;
-            }
+            Console.Clear();
+            Console.WriteLine("¡Game Over!");
+            Environment.Exit(0);
         }
+
+        // Mover la serpiente
+        snake.Insert(0, nuevaCabeza);
+
+        // Verificar si la serpiente come la comida
+        if (nuevaCabeza == comida)
+        {
+            GenerarComida();
+        }
+        else
+        {
+            snake.RemoveAt(snake.Count - 1); // Eliminar la última parte de la serpiente si no come
+        }
+    }
+
+    static void GenerarComida()
+    {
+        int x, y;
+        do
+        {
+            x = random.Next(1, alto - 1);
+            y = random.Next(1, ancho - 1);
+        } while (snake.Contains((x, y)));
+
+        comida = (x, y);
     }
 }
